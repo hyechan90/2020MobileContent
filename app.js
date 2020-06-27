53b4239c7b4fa93e50961ec2b221303a70b5c155
hyechan90 <hyechan90@naver.com>
2020-06-28 01:42:03 +0900

    changed db

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.post('/register', async (req, res) => {
 })
 
 mongoose.connect(
-	'mongodb://localhost:27017/mocon',
+	'mongodb://localhost:27017/test',
 	{ useNewUrlParser: true, useUnifiedTopology: true },
 	() => {
 		console.log('connected to DB')
